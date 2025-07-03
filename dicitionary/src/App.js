607e9b2c6b98f07c05148aba51ebce008efd86fb
Mahima <priyanshisrivastava708016@gmail.com>
2025-07-03 15:06:31 +0530

    search dictionary

--- dicitionary/src/App.js
+++ dicitionary/src/App.js
@@ -1,9 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box,Stack } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
+  const[input,setInput]=useState('');
+  const[dictionary,setDictionary]=useState("");
+     const data = 
+    [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+     ]
+     const handleChange=(e)=>{
+      setInput(e.target.value);
+     }
+     const handleClick=()=>{
+      // data.filter(obj => obj.word===input && (showValue=obj.meaning));
+      const match = data.find(obj=>obj.word.toLowerCase()===input.toLowerCase());
+      if(match){
+        setDictionary(match.meaning);
+      }else{
+            setDictionary("Word not found in the dictionary.");
+      }
+      // console.log(data,showValue);
+     }
   return (
-    <div>git</div>
+    <Stack width={'40%'} padding={'10px'}>
+      <h1>Dicitionary App</h1>
+      <Box>
+      <input type="text" placeholder="Search for a word..." style={{padding:"5px"}} onChange={(e)=>handleChange(e)}/>
+      <button onClick={handleClick}>Search</button>
+      </Box>
+      <h3>Definition:</h3>
+      <p>{dictionary}</p>
+    </Stack>
   );
 }
 
